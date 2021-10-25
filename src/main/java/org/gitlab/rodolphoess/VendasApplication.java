8ce4f4053a94d507cd382bb4e74228d55f361459
Rodolpho Erick <rodolphoerick90@gmail.com>
2021-10-25 07:25:32 -0300

    Adicionando Spring Boot Web e hello world.

--- src/main/java/org/gitlab/rodolphoess/VendasApplication.java
+++ src/main/java/org/gitlab/rodolphoess/VendasApplication.java
@@ -2,9 +2,17 @@ package org.gitlab.rodolphoess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String get() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
